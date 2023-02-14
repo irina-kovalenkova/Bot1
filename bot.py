37436939a8528d4172465f210829e41e4eccbbd9
Iren <kovalenkovaiv@gmail.com>
2023-02-14 10:29:41 +0400

    Bot1

---
+++ bot.py
@@ -0,0 +1,68 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher, types
+
+from config_reader import config
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=config.bot_token.get_secret_value())
+dp = Dispatcher()
+
+
+# Хэндлер на команду /start
+@dp.message(commands=["start"])
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+
+# Хэндлер на команду /test1
+@dp.message(commands=["test1"])
+async def cmd_test1(message: types.Message):
+    await message.answer("Test 1")
+
+
+# Хэндлер на команду /test2
+# Без декоратора, т.к. регистрируется ниже в функции main()
+async def cmd_test2(message: types.Message):
+    await message.reply("Test 2")
+
+
+@dp.message(commands=["answer"])
+async def cmd_answer(message: types.Message):
+    await message.answer("Это простой ответ")
+
+
+@dp.message(commands=["reply"])
+async def cmd_reply(message: types.Message):
+    await message.reply('Это ответ с "ответом"')
+
+
+@dp.message(commands=["dice"])
+async def cmd_dice(message: types.Message):
+    await message.answer_dice(emoji="🎲")
+
+
+@dp.message(commands=["add_to_list"])
+async def cmd_add_to_list(message: types.Message, mylist: list[int]):
+    mylist.append(7)
+    await message.answer("Добавлено число 7")
+
+
+@dp.message(commands=["show_list"])
+async def cmd_show_list(message: types.Message, mylist: list[int]):
+    await message.answer(f"Ваш список: {mylist}")
+
+
+async def main():
+    # Регистрируем хэндлер cmd_test2 по команде /start
+    dp.message.register(cmd_test2, commands=["test2"])
+
+    # Запускаем бота и пропускаем все накопленные входящие
+    # Да, этот метод можно вызвать даже если у вас поллинг
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, mylist=[1, 2, 3])
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ config.py
@@ -0,0 +1,2 @@
+# Bot_kompot = "6003758102:AAHLzPd8wXnToG4oP6G_5BzPlbEw3-g7-ag"
+# admin_ID = 1199893206
